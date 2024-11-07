c836d6f75bbacf3ae4bf1a843d7cf2a181a162b1
Felipe Gouveia <felipe.gouveia@infobase.com.br>
2024-11-07 18:16:05 +0000

    Adds internet permission

--- app/src/main/java/com/felipegouveia/dogchallenge/ui/feature/breeds/view/BreedsView.kt
+++ app/src/main/java/com/felipegouveia/dogchallenge/ui/feature/breeds/view/BreedsView.kt
@@ -1,15 +1,12 @@
 package com.felipegouveia.dogchallenge.ui.feature.breeds.view
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import com.felipegouveia.dogchallenge.ui.common.ErrorView
 import com.felipegouveia.dogchallenge.ui.common.LoadingView
@@ -18,18 +15,14 @@ import com.felipegouveia.dogchallenge.ui.state.UiState
 
 @Composable
 fun BreedsView(viewModel: BreedsViewModel = hiltViewModel()) {
-    val uiState by viewModel.uiState.collectAsState(initial = UiState.Loading)
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     Column {
-        AnimatedVisibility(
-            visible = uiState is UiState.Loading,
-            enter = fadeIn(),
-            exit = fadeOut()
-        ) {
+        AnimatedVisibility(visible = uiState is UiState.Loading) {
             LoadingView()
         }
 
-        AnimatedVisibility(visible = uiState is UiState.Error, enter = fadeIn(), exit = fadeOut()) {
+        AnimatedVisibility(visible = uiState is UiState.Error) {
             ErrorView { viewModel.listBreeds() }
         }
     }
